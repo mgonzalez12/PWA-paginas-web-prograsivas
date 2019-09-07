7d50eb66071db82ab348ba4808f9f7e382e3c5fb
mgonzalez12 <ing.marcosgonzalez1@gmail.com>
2019-09-07 13:22:04 -0500

    service worker

--- 03-service-worker/js/app.js
+++ 03-service-worker/js/app.js
@@ -1,6 +1,26 @@
-
-
 // Detectar si podemos usar Service Workers
 if ( navigator.serviceWorker ) {
-    navigator.serviceWorker.register('/sw.js');
+    navigator.serviceWorker.register('/sw.js')
+    .then(reg => {
+        /*
+        setTimeout(()=>{
+            reg.sync.register('posteo-gatitos');
+            console.log('se enviarion fotos');
+        },3000);
+        */
+
+        Notification.requestPermission().then(result =>{
+            console.log(result);
+            reg.showNotification('Hola Mundo');
+        });
+    });
 }
+
+
+
+/*
+fetch('https://reqres.in/api/users')
+.then( resp => resp.text())
+.then(console.log);
+
+*/
--- 03-service-worker/sw.js
+++ 03-service-worker/sw.js
@@ -1,3 +1,39 @@
 
 // Ciclo de vida del SW
 
+self.addEventListener('install',event => {
+    console.log('hola sw'); 
+
+    setTimeout(()=>{
+        console.log('SW: Instalaciones terminadas.');
+    });
+
+     self.skipWaiting();  // actualiza automaticamente el SW
+});
+
+//  cuando se activa
+self.addEventListener('activate',event =>{
+
+    console.log('activo service worker 2');
+});
+
+// Manejo de peticiones
+self.addEventListener('fetch',event =>{
+
+    //console.log('SW:',event.request.url);
+});
+
+
+// SYNC: Recuperamos conexion a internet
+self.addEventListener('sync',event =>{
+    console.log('Tenemos conexion');
+    console.log(event);
+    console.log(event.tag);
+});
+
+// PUSH : manejar las push
+
+self.addEventListener('push',event =>{
+    console.log('push');
+});
+
